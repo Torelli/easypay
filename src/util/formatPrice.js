1e7063780cae576628a94c2fca0a701ea1dec228
Giovanni Torelli <giovanni.walter@outlook.com>
2023-12-15 15:00:23 -0300

    Created utility function to format price

---
+++ src/util/formatPrice.js
@@ -0,0 +1,8 @@
+export default function formatPrice(price) {
+    let USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+
+    return USDollar.format(price);
+}

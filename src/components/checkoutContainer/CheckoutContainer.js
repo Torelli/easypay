0e913597d41c4ad35463318bde38811afc75580f
Giovanni Torelli <giovanni.walter@outlook.com>
2023-12-15 01:32:04 -0300

    Updated CheckoutCotnainer style and added form and button container components

--- src/components/checkoutContainer/CheckoutContainer.js
+++ src/components/checkoutContainer/CheckoutContainer.js
@@ -1,11 +1,17 @@
+import ButtonContainer from "../buttonContainer/ButtonContainer";
 import Cart from "../cart/Cart";
+import Form from "../form/Form";
 
 export default function CheckoutContainer() {
-    const container = document.createElement("div");
-    container.setAttribute("class", "mt-56 w-1/3 p-4 bg-gray-200 drop-shadow-2xl rounded-md");
+  const container = document.createElement("div");
+  container.setAttribute(
+    "class",
+    "grid grid-cols-2 gap-2 mt-36 w-1/2 p-4 bg-gray-200 drop-shadow-2xl rounded-md"
+  );
 
-    container.appendChild(Cart());
+  container.appendChild(Cart());
+  container.appendChild(Form());
+  container.appendChild(ButtonContainer());
 
-    return container;
-
+  return container;
 }
